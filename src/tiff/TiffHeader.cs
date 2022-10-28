9452d5dfd30364457c8f820dddefadcd7147e83b
Guillaume Pelletier <guillaume.pelletier@dotvision.com>
2022-10-28 11:34:42 +0200

    initial code commit

---
+++ src/tiff/TiffHeader.cs
@@ -0,0 +1,19 @@
+﻿
+namespace IOfThings.Media
+{
+    public class TifHeader
+    {
+        public const ushort LittleEndian = (ushort)0x4949;
+        public const ushort BigEndian = (ushort)0x4D4D;
+        public const byte Identification = 0x2A; 
+
+        public ushort  Identifier;
+        public ushort  Version;
+        public uint    IFDOffset;
+    
+        public override string ToString()
+        {
+            return "${{id:{Identifier},ver:{Version},ifdo:{IFDOffset}}}";
+        }
+    }
+}
